713fe58ed7077b3cc89370cd91e4b32eb6d77861
mahmudul121 <shohag12145@gmail.com>
2023-11-01 20:12:35 +0600

    first init

---
+++ src/pages/home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Home.scss'
+
+const Home = () => {
+  return (
+    <div className='app-layout home-wrapper'>
+      <h1>Home Page t3</h1>
+    </div>
+  )
+}
+
+export default Home
