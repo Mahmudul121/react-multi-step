457dafbf326abe4aff66536beb770f3acd2839ac
mahmudul121 <shohag12145@gmail.com>
2023-11-05 21:28:10 +0600

    pdf generator

--- src/pages/result/Result.js
+++ src/pages/result/Result.js
@@ -6,6 +6,50 @@ import Button from "../../components/ui/Button";
 import { useLocation, useNavigate } from "react-router-dom";
 import { toastify } from "../../components/ui/Toast";
 
+// pdf
+import ReactPDF, {
+  PDFDownloadLink,
+  PDFViewer,
+  Page,
+  Text,
+  View,
+  Document,
+  StyleSheet,
+} from "@react-pdf/renderer";
+
+const styles = StyleSheet.create({
+  page: {
+    flexDirection: "row",
+    backgroundColor: "#fff",
+  },
+  table: {
+    width: "100%",
+    margin: 10,
+    padding: 10,
+    flexGrow: 1,
+  },
+  row: {
+    display: "flex",
+    flexDirection: "row",
+    borderTop: "1px solid #EEE",
+    paddingTop: 8,
+    paddingBottom: 8,
+  },
+  header: {
+    borderTop: "none",
+  },
+  bold: {
+    fontWeight: "bold",
+  },
+  // So Declarative and unDRY 👌
+  row1: {
+    width: "50%",
+  },
+  row2: {
+    width: "50%",
+  },
+});
+
 const Result = () => {
   const location = useLocation();
   const navigate = useNavigate();
@@ -21,9 +65,7 @@ const Result = () => {
     }
     // eslint-disable-next-line
   }, [location.state]);
-  const handleClickPDF = () => {
-    console.log("asd");
-  };
+  console.log(resultData, resultData?.length);
   return (
     <div className="app-layout result-page">
       <Container>
@@ -69,14 +111,113 @@ const Result = () => {
             </tr>
           </tbody>
         </Table>
+        {/* pdf generator */}
         <div className="my-5 text-center">
-          <Button
-            disabled={resultData?.length}
-            onClick={() => handleClickPDF()}
-            className="w-50 mt-4"
-            type={"button"}
-            name={"PDF"}
-          />
+          {resultData?.project_name ? (
+            <PDFDownloadLink
+              className="w-50 mt-4 btn btn-primary"
+              document={
+                <Document>
+                  <Page size="A4" style={styles.page}>
+                    <View style={styles.table}>
+                      <View style={[styles.row, styles.bold, styles.header]}>
+                        <Text style={styles.row1}>
+                          <Text style={styles.bold}>Project Name</Text>
+                        </Text>
+                        <Text style={styles.row1}>
+                          {resultData?.project_name ?? "-"}
+                        </Text>
+                      </View>
+                      {/* client */}
+                      <View style={[styles.row, styles.bold, styles.header]}>
+                        <Text style={styles.row1}>
+                          <Text style={styles.bold}>Client</Text>
+                        </Text>
+                        <Text style={styles.row1}>
+                          {resultData?.client ?? "-"}
+                        </Text>
+                      </View>
+                      {/* cc */}
+                      <View style={[styles.row, styles.bold, styles.header]}>
+                        <Text style={styles.row1}>
+                          <Text style={styles.bold}>Contractor</Text>
+                        </Text>
+                        <Text style={styles.row1}>
+                          {resultData?.contractor ?? "-"}
+                        </Text>
+                      </View>
+                      {/* description */}
+                      <View style={[styles.row, styles.bold, styles.header]}>
+                        <Text style={styles.row1}>
+                          <Text style={styles.bold}>Project Description</Text>
+                        </Text>
+                        <Text style={styles.row1}>
+                          {resultData?.project_description ?? "-"}
+                        </Text>
+                      </View>
+                      {/* x min */}
+                      <View style={[styles.row, styles.bold, styles.header]}>
+                        <Text style={styles.row1}>
+                          <Text style={styles.bold}>MIN X</Text>
+                        </Text>
+                        <Text style={styles.row1}>
+                          {resultData?.min_x ?? 0}
+                        </Text>
+                      </View>
+                      {/* x max */}
+                      <View style={[styles.row, styles.bold, styles.header]}>
+                        <Text style={styles.row1}>
+                          <Text style={styles.bold}>MAX X</Text>
+                        </Text>
+                        <Text style={styles.row1}>
+                          {resultData?.max_x ?? 0}
+                        </Text>
+                      </View>
+                      {/* y */}
+                      <View style={[styles.row, styles.bold, styles.header]}>
+                        <Text style={styles.row1}>
+                          <Text style={styles.bold}>MIN Y</Text>
+                        </Text>
+                        <Text style={styles.row1}>
+                          {resultData?.min_y ?? 0}
+                        </Text>
+                      </View>
+                      {/* y max */}
+                      <View style={[styles.row, styles.bold, styles.header]}>
+                        <Text style={styles.row1}>
+                          <Text style={styles.bold}>MAX Y</Text>
+                        </Text>
+                        <Text style={styles.row1}>
+                          {resultData?.max_y ?? 0}
+                        </Text>
+                      </View>
+                      {/* z */}
+                      <View style={[styles.row, styles.bold, styles.header]}>
+                        <Text style={styles.row1}>
+                          <Text style={styles.bold}>MIN Z</Text>
+                        </Text>
+                        <Text style={styles.row1}>
+                          {resultData?.min_z ?? 0}
+                        </Text>
+                      </View>
+                      {/* z max */}
+                      <View style={[styles.row, styles.bold, styles.header]}>
+                        <Text style={styles.row1}>
+                          <Text style={styles.bold}>MAX Z</Text>
+                        </Text>
+                        <Text style={styles.row1}>
+                          {resultData?.max_z ?? 0}
+                        </Text>
+                      </View>
+                    </View>
+                  </Page>
+                </Document>
+              }
+              fileName="report.pdf"
+            >
+              Download PDF
+            </PDFDownloadLink>
+          ) : null}
         </div>
       </Container>
     </div>
